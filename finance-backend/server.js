b5177bf0587bfeed2fd50460318c38c7ac6873b5
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2024-06-13 21:32:38 +0300

    add cors

--- finance-backend/server.js
+++ finance-backend/server.js
@@ -5,9 +5,12 @@ const authRoutes = require('./routes/auth');
 const debtRoutes = require('./routes/debts');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
